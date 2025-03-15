59a413c34e04d5fa7a99d2c89aeab3ba0d4a2afe
sashavershkova <a.m.vershkova@gmail.com>
2025-03-15 12:59:05 -0700

    passed all tests

--- part_one.py
+++ part_one.py
@@ -11,9 +11,9 @@ def driver():
     ]
 
     categorized_sequences = {}
-    categorized_sequences["undetermined"] = [] # strands that can't be determined
-    categorized_sequences["dna"] = [] # dna strands
-    categorized_sequences["rna"] = [] # rna strands
+    categorized_sequences[-1] = [] # strands that can't be determined
+    categorized_sequences[0] = [] # dna strands
+    categorized_sequences[1] = [] # rna strands
 
     for sequence in all_sequences:
         category = categorize_strand(sequence)
@@ -58,7 +58,7 @@ def categorize_strand(strand):
         return -1
 
     return 0 if is_t_present else 1
-
+    
 def encode_strand(strand):
     if not strand:
         return ""
@@ -69,10 +69,12 @@ def encode_strand(strand):
     for index in range(1, len(strand)):
         if strand[index - 1] == strand[index]:
             count += 1
-        else:
-            new_entry = strand[index - 1] + count
-            encoding.append(new_entry)
+        else: 
+            new_entry = strand[index - 1] + str(count)
+            encoding.append(new_entry)         
             count = 1
+        
+    encoding.append(strand[-1] + str(count))
 
     return "".join(encoding)
 
--- sequence_tests.py
+++ sequence_tests.py
@@ -1,8 +1,8 @@
 from part_one import categorize_strand, decode_strand, encode_strand
 from part_two import categorize_strand as categorize_encoded_strand
 
-DNA = "GGGGGAAAGGCCCCTTTAAAACCCCTTTTTAAAACCCCCGGGAAAATTTTAAA"
-DNA_ENCODED = "G5A3G2C4T3A4C4T5A4C5G3A4T4A3"
+DNA = "GGGGGAAAGGCCCCTTTAAAACCCCTTTTTAAAACCCCCGGGAAAATTTTAAA" 
+DNA_ENCODED = "G5A3G2C4T3A4C4T5A4C5G3A4T4A3" 
 RNA = "CCCAAAAAUUUUCCCCGGGUUAAAAUUUUUGGGGGAAACCCGGGGAAAACCCCC"
 RNA_ENCODED= "C3A5U4C4G3U2A4U5G5A3C3G4A4C5"
 STRAND_BOTH_BASES = "GGGGGAAAUUTTTCCCCAAAACCCCUUUUUAAAATTTTCCCCCAAAAGGGAAA"
@@ -10,9 +10,10 @@ ENCODED_BOTH_BASES = "G5A3U2T3C4A4C4U5A4T4C5A4G3A3"
 STRAND_NEITHER_BASE = "CCCAAAAAGGGGCCCCCGGGGAAAACCCCGGGGGAAACCCGGGGAAAACCCCC"
 ENCODED_NEITHER_BASE = "C3A5G4C5G4A4C4G5A3C3G4A4C5"
 
+
 def tests():
     part_one_tests()
-    # part_two_tests()
+    part_two_tests()
     print("All tests passed!\n")
 
 def part_one_tests():
@@ -20,7 +21,7 @@ def part_one_tests():
     rna_tests()
     uncategorized_tests()
 
-def dna_tests():
+def dna_tests():  
     result = encode_strand(DNA)
     assert result == DNA_ENCODED
 
